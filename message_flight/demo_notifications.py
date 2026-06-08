f35ad318bf4efb2433ca15e68a671fcec6d81547
wx528 <33819629+wx528@users.noreply.github.com>
2026-06-09 05:25:16 +0800

    refactor: extract NOTIFICATIONS demo list into demo_notifications module

---
+++ message_flight/demo_notifications.py
@@ -0,0 +1,12 @@
+"""Demo notification pool used when winsdk is unavailable."""
+
+NOTIFICATIONS = [
+    "Meeting with Andrew in 5 min",
+    "You have a new message from Mom",
+    "Lunch time!",
+    "Stand-up meeting starts now",
+    "Don't forget to drink water",
+    "Code review requested by Tom",
+    "Daily report due in 30 min",
+    "New email: Project Update",
+]
